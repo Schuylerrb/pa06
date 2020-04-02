d2de21e18a3570d78f7aa7ee87e4380236406039
Schuylerrb <schuylerrb@gmail.com>
2020-04-02 00:19:40 -0400

    new folder with almost final program

---
+++ pa06_hangman_REV/.ipynb_checkpoints/hangman_webapp-checkpoint.py
@@ -0,0 +1,106 @@
+"""
+  website_demo shows how to use templates to generate HTML
+  from data selected/generated from user-supplied information
+"""
+
+from flask import Flask, render_template, request
+import hangman_app
+app = Flask(__name__)
+
+global state
+state = {'guesses':[],
+		 'word':[],
+		 'word_so_far':"-----------",
+		 'done':False,
+		 'error':"",
+		 'guessesLeft':8
+         }
+
+@app.route('/')
+@app.route('/main')
+def main():
+	return render_template('hangman.html')
+
+@app.route('/start')
+def play():
+	global state
+	state = {'guesses':[],
+			 'word':[],
+			 'word_so_far':"-----------",
+			 'done':False,
+			 'error':"",
+			 'guessesLeft': 8,
+			 'won': False
+			 }
+	newWord = hangman_app.generate_random_word()
+	for i in newWord:
+		state['word'].append(i)
+	state['guesses'] = []
+	state['word_so_far'] = hangman_app.coverMystery(state['word'])
+	return render_template("start.html",state=state)
+
+@app.route('/play',methods=['GET','POST'])
+def hangman():
+	""" plays hangman game """
+	global state
+	if request.method == 'GET':
+		return play()
+
+	elif request.method == 'POST':
+		letter = request.form['guess'].lower()
+		if state['done'] == True and state['won'] == False:
+			if letter == "again":
+				return play()
+			else:
+				state['error'] = "game is over, type again to play again, if u dont want to get out"
+				return render_template('play.html',state=state)
+		elif state['done'] == True and state['won'] == True:
+			if letter == "again":
+				return play()
+			else:
+				state['error'] = "you won, type again in form to play again, if not get out"
+				return render_template('play.html',state=state)
+		if letter in state['guesses']:
+			state['error'] = "chose a new letter"
+		elif len(letter) != 1:
+			state['error'] = "Please guess 1 letter"
+		elif letter not in "qwertyuiopasdfghjklzxcvbnm":
+			state['error'] = "Please guess a letter"
+		else:
+			state['guesses'] += [letter]
+			state['guessesLeft'] -= 1
+			print(state)
+			print(state['word_so_far'] != state['word'])
+			if (state['guessesLeft'] == 0 and state['word_so_far'] != state['word']):
+				state['done'] = True
+				state['error'] = "you lost sorry, if you wish to play again simply type again in the form"
+
+				#if len(letter) != 1:
+					#state['error'] = "Please guess 1 letter"
+				#elif letter not in "qwertyuiopasdfghjklzxcvbnm":
+					#state['error'] = "Please guess a letter"
+			elif letter in state['word']:
+				state['word_so_far'] = hangman_app.uncoverLetter(letter,state['word'],state['word_so_far'])
+				state['error'] = "Congrats"
+				state['guessesLeft'] += 1
+				if (state['guessesLeft'] >= 0 and state['word_so_far'] == state['word']):
+					state['done'] = True
+					state['won'] = True
+					state['error'] = "you have won! to play again type again in form"
+			else:
+				state['error'] = "Try again"
+
+
+		# check if letter has already been guessed
+		# and generate a response to guess again
+		# else check if letter is in word
+		# then see if the word is complete
+		# if letter not in word, then tell them
+
+		return render_template('play.html',state=state)
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=3000)
---
+++ pa06_hangman_REV/hangman_app.py
@@ -0,0 +1,35 @@
+"""
+   hangman_app.py is an app for playing hangman in the terminal
+   it is also used as a module in the hangman_webapp flask app
+"""
+
+def generate_random_word():
+   import random
+   wordslist_demo = "acres adult advice arrangement attempt August Autumn border breeze brick calm canal Casey cast claws coach constantly contrast cookies customs damage Danny deeply depth discussion doll donkey Egypt Ellen essential exchange exist explanation facing film finest fireplace floating folks fort garage grabbed grandmother habit happily Harry heading hunter Illinois image independent instant January kids label Lee lungs manufacturing Martin mathematics melted memory mill mission monkey Mount mysterious neighborhood Norway nuts occasionally official ourselves palace Pennsylvania Philadelphia plates poetry policeman positive possibly practical pride promised recall relationship remarkable require rhyme rocky rubbed rush sale satellites satisfied scared selection shake shaking shallow shout silly simplest slight slip slope soap solar species spin stiff swung tales thumb tobacco toy trap treated tune University vapor vessels wealth wolf zoo".split()
+   word_demo = random.choice(wordslist_demo)
+   word_demo = word_demo.lower()
+   word1 = word_demo
+   return word1
+
+def coverMystery(word_):
+    mysteryWord = list("-"*len(word_))
+    return mysteryWord
+
+def uncoverLetter(letter_,wordList,coverList):
+    counter = -1
+    for x in wordList:
+        counter += 1
+        if x == letter_:
+            coverList[counter] = wordList[counter]
+        else:
+            continue
+    return coverList
+
+
+
+def play_hangman():
+   """ this is the python script version of the game """
+   print("The hangman app is under construction. Try again later!")
+
+if __name__ == '__main__':
+    play_hangman()
